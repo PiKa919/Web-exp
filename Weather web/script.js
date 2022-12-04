40f0e9b98fd092a30cc319528a3da17242bdd12f
Ankit Das <96786190+PiKa919@users.noreply.github.com>
2022-12-05 00:27:01 +0530

    Add files via upload

---
+++ Weather web/script.js
@@ -0,0 +1,24 @@
+let weather = {
+    "apiKey":"dc98ac3962f54f07f77417ef79a80d89",
+    fetchWeather: function() {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=metric&appid"+ this.apiKey
+        )
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, description, temp, humidity, speed);
+
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png"; 
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/h";
+    }
+};
